8397afaccffcd568e6ffe9c4f9f4f014dad047b7
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-06-21 18:57:41 -0300

    Exercicio 1 operações aritiméticas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript/script.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 6;
+
+let soma = (a+b);
+console.log(soma);
+
+let subtração = (a-b);
+console.log(subtração);
+
+let multiplicação = (a*b);
+console.log(multiplicação);
+
+let divisão = (a/b);
+console.log(divisão);
+
+let resto = (a%b);
+console.log(resto);
