4f3da87c469dc232dd97b851de7f3eeab0b8bafc
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-06-22 19:30:30 -0300

    exercicio 9, dividindo todo array por 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript/script2.js
@@ -1,9 +1,7 @@
 let numbers = [];
-
+let divisao = (numbers[index] / 2);
 for (let index = 0; index <= 25; index += 1) {
-
-
 numbers.push(index) 
-}
 
-console.log(numbers)
+}
+console.log(divisao);
