5c00e2befc78f2829f10bab3fc214f97c169220a
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-06-22 18:59:42 -0300

    Exercicio 8, criando um array de 1 a 25

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript/script2.js
@@ -1,9 +1,9 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorNumero = numbers[0]
+let numbers = [];
 
-for (let index = 0; index < numbers.length; index += 1)
-if (numbers[index] < menorNumero) {
-    menorNumero = numbers[index];
+for (let index = 0; index <= 25; index += 1) {
 
+
+numbers.push(index) 
 }
-console.log(menorNumero);
+
+console.log(numbers)
