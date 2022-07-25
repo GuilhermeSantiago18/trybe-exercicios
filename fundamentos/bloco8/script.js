2d5d55aa05832492885da95c9435236a4089b4a2
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-07-25 15:42:36 -0300

    exercicio 2, bloco 8.3

--- fundamentos/bloco8/script.js
+++ fundamentos/bloco8/script.js
@@ -1,65 +1,30 @@
-const books = [
+const expectedResult = [
   {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
+    age: 31,
+    author: 'Isaac Asimov',
   },
   {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
+    age: 38,
+    author: 'H. P. Lovecraft',
   },
   {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
+    age: 39,
+    author: 'Stephen King',
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
+    age: 43,
+    author: 'George R. R. Martin',
   },
   {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: {
-      name: 'Stephen King',
-      birthYear: 1947,
-    },
-    releaseYear: 1986,
+    age: 45,
+    author: 'Frank Herbert',
   },
   {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: {
-      name: 'H. P. Lovecraft',
-      birthYear: 1890,
-    },
-    releaseYear: 1928,
+    age: 62,
+    author: 'J. R. R. Tolkien',
   },
 ];
-const reBook = books.map((element) => `${element.name} ${element.genre} ${element.author.name}`);
-console.log(reBook);
-// Adicione o código do exercício aqui:
+
+const verify = expectedResult.map((element) => element); 
+verify.sort((a,b) => a.age - b.age);
+console.log(verify);
