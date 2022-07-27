bb0fc88061272776db119195000a0bdb3611f2c1
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-07-27 18:40:20 -0300

    Exercicio2 , bloco 8.5

--- fundamentos/bloco8/script.js
+++ fundamentos/bloco8/script.js
@@ -1,11 +1,4 @@
-const rectangleArea = (width, height) => width * height;
+const sum = ((...numbers) => numbers
+.reduce((acc, curr) => acc + curr, 0));
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
-
-rectangles.forEach((rectangle) => {
-  rectangleArea(...rectangle) // altere a chamada da funcao rectangleArea
-  console.log(rectangle[0] * rectangle[1]);
-});
+console.log(sum(1,2,3,4,23,1,4,13,23,1));
