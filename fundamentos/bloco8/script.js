33bdf5579aaa1586426ee72269376ae3533414fc
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-07-27 20:07:56 -0300

    Exercicio 5, bloco 8.5

--- fundamentos/bloco8/script.js
+++ fundamentos/bloco8/script.js
@@ -1,36 +1,4 @@
-const people = [
-  {
-    name: 'Nicole',
-    bornIn: 1992,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Harry',
-    bornIn: 2008,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Toby',
-    bornIn: 1901,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Frida',
-    bornIn: 1960,
-    nationality: 'Dannish',
-  },
-  {
-    name: 'Fernando',
-    bornIn: 2001,
-    nationality: 'Brazilian',
-  },
-];
+const myList = [1, 2, 3];
 
-const osDois = {nationality, bornIn}
-
-people.filter(({nationality, bornIn}) => nationality === 'Australian' 
-&& bornIn < 2000);
- 
-console.log(filterPeople);
-
-// escreva filterPeople abaixo
+console.log(myList.reverse());
+// escreva swap abaixo
