6c93a08bf9caca9fd7fd3aafe0d531c80bf0bfd0
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-07-27 20:21:21 -0300

    Exercicio 7 , bloco 8.5

--- fundamentos/bloco8/script.js
+++ fundamentos/bloco8/script.js
@@ -1,8 +1,25 @@
-const palio = ['Palio', 'Fiat', 2019];
-const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
-const chiron = ['Chiron', 'Bugatti', 2016];
+const ships = [
+  {
+    name: "Titanic",
+    length: 269.1,
+    measurementUnit: "meters",
+  },
+  {
+    name: "Queen Mary 2",
+    length: 1132,
+    measurementUnit: "feet",
+  },
+  {
+    name: "Yamato",
+    length: 256,
+    measurementUnit: "meters",
+  },
+];
 
-// escreva toObject abaixo
-const toObject = ([name, brand, year]) => ({ name, brand, year });
+// escreva shipLength abaixo
+const shipLength = ({ name, length, measurementUnit }) =>
+  `${name} is ${length} ${measurementUnit} long`;
 
-console.log(toObject(palio));
+console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
