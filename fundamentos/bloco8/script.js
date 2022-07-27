7cfde15593ff74c9f235a46f353569bbca50991e
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-07-27 20:15:41 -0300

    Exercicio 6, bloco 8.5

--- fundamentos/bloco8/script.js
+++ fundamentos/bloco8/script.js
@@ -1,4 +1,8 @@
-const myList = [1, 2, 3];
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
 
-console.log(myList.reverse());
-// escreva swap abaixo
+// escreva toObject abaixo
+const toObject = ([name, brand, year]) => ({ name, brand, year });
+
+console.log(toObject(palio));
