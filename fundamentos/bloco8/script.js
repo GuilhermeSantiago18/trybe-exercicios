ddc6945a42b5f857575537115d1df453bc4697c1
Guilherme Santiago <guilherme.limasantiago@gmail.com>
2022-07-28 00:27:21 -0300

    Exercise  9 - object destructuring and spread operator

--- fundamentos/bloco8/script.js
+++ fundamentos/bloco8/script.js
@@ -1,25 +1,11 @@
-const ships = [
-  {
-    name: "Titanic",
-    length: 269.1,
-    measurementUnit: "meters",
-  },
-  {
-    name: "Queen Mary 2",
-    length: 1132,
-    measurementUnit: "feet",
-  },
-  {
-    name: "Yamato",
-    length: 256,
-    measurementUnit: "meters",
-  },
-];
 
-// escreva shipLength abaixo
-const shipLength = ({ name, length, measurementUnit }) =>
-  `${name} is ${length} ${measurementUnit} long`;
+const yearSeasons = {
+    spring: ['March', 'April', 'May'],
+    summer: ['June', 'July', 'August'],
+    autumn: ['September', 'October', 'November'],
+    winter: ['December', 'January', 'February'],
+  };
+  const { spring, summer, autumn, winter } = yearSeasons;
+  const months = [...spring, ...summer, ...autumn, ...winter];
 
-console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+  console.log(months);
